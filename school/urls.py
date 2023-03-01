f5199f541ae4161dc4a09eaaeb0b471032af1fd5
ashutosh sahu <anshitsani100@gmail.com>
2023-03-01 18:03:14 +0530

    fix

--- school/urls.py
+++ school/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(templates_name='home.html')),
-path('about/', TemplateView.as_view(templates_name='about.html')),
-path('privacy/', TemplateView.as_view(templates_name='privacy.html')),
+    path('',TemplateView.as_view(template_name='home.html')),
+path('about/', TemplateView.as_view(template_name='about.html')),
+path('privacy/', TemplateView.as_view(template_name='privacy.html')),
 ]
